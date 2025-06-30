b5399d02090f96ae671c6b45d1cfe606193a4ca1
euxinus <2216771994@qq.com>
2025-06-30 14:28:22 +0800

    因子和策略python计算核心端初步创建

---
+++ factor/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from app.api.v1.routers import api_router
+from app.core.config import settings
+
+app = FastAPI(
+    title=settings.PROJECT_NAME, 
+    openapi_url=f"{settings.API_V1_STR}/openapi.json"
+)
+
+app.include_router(api_router, prefix=settings.API_V1_STR)
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Quant Engine!"}
