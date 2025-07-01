0551a367b10918818791dbadc77cef6f88558a0d
StoneXie <2323604986@qq.com>
2025-07-01 14:16:32 +0800

    增加了组合产品管理子系统的sql查询

---
+++ backend/src/main/java/org/project/backend/mapper/ProductComboFundMapper.java
@@ -0,0 +1,28 @@
+package org.project.backend.mapper;
+
+import org.apache.ibatis.annotations.*;
+import org.project.backend.entity.ProductComboFund;
+import java.util.List;
+
+@Mapper
+public interface ProductComboFundMapper {
+
+    @Insert("INSERT INTO product_combo_fund " +
+            "(combo_id, fund_code, fund_name, weight, position) " +
+            "VALUES (#{comboId}, #{fundCode}, #{fundName}, #{weight}, #{position})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    int insert(ProductComboFund comboFund);
+
+    @Update("UPDATE product_combo_fund SET fund_code=#{fundCode}, fund_name=#{fundName}, " +
+            "weight=#{weight}, position=#{position} WHERE id=#{id}")
+    int update(ProductComboFund comboFund);
+
+    @Select("SELECT * FROM product_combo_fund WHERE combo_id=#{comboId}")
+    List<ProductComboFund> selectByComboId(Long comboId);
+
+    @Delete("DELETE FROM product_combo_fund WHERE id=#{id}")
+    int deleteById(Long id);
+
+    @Delete("DELETE FROM product_combo_fund WHERE combo_id=#{comboId}")
+    int deleteByComboId(Long comboId);
+}
